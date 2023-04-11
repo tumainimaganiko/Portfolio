26ce99537efd89349f67ba872b3900f5ba54e84b
tumainimaganiko <tumainimaganiko@gmail.com>
2023-04-11 11:50:18 +0300

    Fix linters error

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-let btnMenu = document.getElementById('hamburger');
-let xBtn = document.getElementById("x-mark");
+const btnMenu = document.getElementById('hamburger');
+const xBtn = document.getElementById('x-mark');
 
-btnMenu.addEventListener('click',myFunction);
-
-function myFunction(){
-    let element = document.querySelector(".toolbar");   
-    let list = document.getElementById('option');
-    element.style.display = "none";
-    list.style.display = "block";
+function myFunction() {
+  const element = document.querySelector('.toolbar');
+  const list = document.getElementById('option');
+  element.style.display = 'none';
+  list.style.display = 'block';
 }
+
+function closing() {
+  const element = document.querySelector('.toolbar');
+  const list = document.getElementById('option');
+  element.style.display = 'flex';
+  list.style.display = 'none';
+}
+
+btnMenu.addEventListener('click', myFunction);
 
 xBtn.addEventListener('click', closing);
 
-function closing(){
-    let element = document.querySelector(".toolbar");   
-    let list = document.getElementById('option');
-    element.style.display = "flex";
-    list.style.display = "none";
-}
-
-let btn1 = document.getElementById('about-link');
-let btn2 = document.getElementById('contact-link');
-let btn3 = document.getElementById('port');
-
+const btn1 = document.getElementById('about-link');
+const btn2 = document.getElementById('contact-link');
+const btn3 = document.getElementById('port');
 
 btn1.addEventListener('click', closing);
-btn2.addEventListener('click',closing);
-btn3.addEventListener('click',closing);
+btn2.addEventListener('click', closing);
+btn3.addEventListener('click', closing);
